23cf6b1acf047a6eb93d27a4af8e0df748b29f48
marimaari <125379781+marimaari@users.noreply.github.com>
2024-04-24 14:04:14 -0300

    001

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const botoes = document.querySelectorAll('.botao');
+const abasConteudo = document.querySelectorAll('.aba-conteudo-item')
 console.log(botoes)
 
-for (let i=0;i<botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j=0;j< botoes.length; j++) {
-            botoes[j].classList.remove('ativo');
-        }
+for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
+
+    botoes[i].addEventListener('click', function{
+
+    for(let j=0;j<botoes.length;j++) { 
+        botoes[j].classList.remove('ativo');
+    }
     botoes[i].classList.add('ativo');
     }
 }
-   
